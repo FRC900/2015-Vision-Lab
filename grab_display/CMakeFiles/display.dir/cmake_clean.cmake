14cacb94c4a967aeee8aea3a6463428e49a54fd5
Adithya Balaji <adithyabsk@gmail.com>
2015-10-26 23:27:15 +0000

    Found the centers of the image

---
+++ grab_display/CMakeFiles/display.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/display.dir/display.cpp.o"
+  "display.pdb"
+  "display"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/display.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
