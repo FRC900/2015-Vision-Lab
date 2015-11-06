a6bc3001a6d07d44b8822c510ad80ba638273e2f
Eric Blau <eblau@eblau.com>
2015-11-05 19:33:38 -0500

    Added Error checking to the ResizeRect function Fixed Average calculation

--- grab_display/display.cpp
+++ grab_display/display.cpp
@@ -25,8 +25,96 @@ int ValMax= 255;
 int ValMin= 190;
 int dilation_size = 0;
 RNG rng(12345);
-
-
+Rect findRect(Mat &input, int HMin, int HMax, int SMin, int SMax, int VMin, int VMax)
+{
+        //splits the input into channels, which it then evaluates by range
+        vector<Mat> channels;
+        split(input, channels);
+        vector<Mat> comp(3);
+        inRange(channels[0], HMin, HMax, comp[0]);
+        inRange(channels[1], SMin, SMax, comp[1]);
+        inRange(channels[2], VMin, VMax, comp[2]);
+        Mat btrack;
+        bitwise_and(comp[0], comp[1], btrack);
+        bitwise_and(btrack, comp[2], btrack);
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        vector<Point> points;
+        findContours( btrack, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        vector<Rect> boundRect( contours.size() );
+        for( int i = 0; i < contours.size(); i++ )
+        {
+            for( int j = 0; j < contours[i].size(); j++)
+            {
+                points.push_back(contours[i][j]);
+            }
+        }
+        return boundingRect(points);
+}
+Rect adjustRect(Rect &input, float ratio)
+{
+    Size rectsize = input.size();
+    int width = rectsize.width;
+    int height = rectsize.height;
+    if (width / ratio > height)
+    {
+        height = width / ratio;
+    }
+    else if (width / ratio < height)
+    {
+        width = height * ratio;
+    }
+    Point tl = input.tl();
+    tl.x = tl.x - (width - rectsize.width)/2;
+    tl.y = tl.y - (height - rectsize.height)/2;
+    Point br = input.br();
+    br.x = br.x + (width - rectsize.width)/2;
+    br.y = br.y + (height - rectsize.height)/2;
+    return Rect(tl, br);
+}
+bool resizeRect(Rect &theRect, float pMax, const Mat imageCool)
+{
+    Point tl = theRect.tl();
+    Point br = theRect.br();
+    int sanityCount = 0;
+    do {
+        // Sanity check to make sure we don't get stuck in an infinite loop
+        // in case we can't adjust the rectangle within the bounds of the Mat.
+        ++sanityCount;
+        int intMax = int(pMax * 100 + .5);
+        float adjust = rng.uniform(0,intMax);
+        Size rectsize = theRect.size();
+        float width = rectsize.width * (1 + adjust/100);
+        int intwidth = int(width+.5);
+        float height = rectsize.height * (1 + adjust/100);
+        int intheight = int(height+.5);
+        tl = theRect.tl();
+        tl.x = tl.x - (width - rectsize.width)/2;
+        tl.y = tl.y - (height - rectsize.height)/2;
+        br = theRect.br();
+        br.x = br.x + (width - rectsize.width)/2;
+        br.y = br.y + (height - rectsize.height)/2;
+    }
+    while ((tl.x < 0 || tl.y < 0 || br.x > imageCool.cols || br.y > imageCool.rows) && sanityCount <= 9000);
+    if (sanityCount > 9000) {
+        cerr << "IT'S OVER 9000!!!" << endl;
+        return false;
+    }
+    theRect = Rect(tl, br);
+    return true;
+}
+Mat rgbValThresh(int HMin,int HMax,int SMin,int SMax,int VMin, int VMax)
+{
+    Mat color = (Mat_<cv::Vec3b>(1,2) << Vec3b(HMin,SMin,VMin), Vec3b(HMax,SMax,VMax));
+    cvtColor(color, color, COLOR_HSV2BGR);
+    int valB = (color.at<cv::Vec3b>(1,1)[0]/2) + (color.at<cv::Vec3b>(1,2)[0])/2;
+    int valG = (color.at<cv::Vec3b>(1,1)[1]/2) + (color.at<cv::Vec3b>(1,2)[1])/2;
+    int valR = (color.at<cv::Vec3b>(1,1)[2]/2) + (color.at<cv::Vec3b>(1,2)[2])/2;
+    int threshB = valB - color.at<cv::Vec3b>(1,1)[0];
+    int threshG = valG - color.at<cv::Vec3b>(1,1)[1];
+    int threshR = valR - color.at<cv::Vec3b>(1,1)[2];
+    return (Mat_<cv::Vec3b>(1,2) << Vec3b(valB,valG,valR), Vec3b(threshB, threshG, threshR));
+}
 int main() {
     namedWindow("Original", WINDOW_AUTOSIZE);
     //namedWindow("Parameters",WINDOW_AUTOSIZE);
@@ -155,6 +243,12 @@ int main() {
         {
             mu[i] = moments( contours[i], false );
         }
+        Rect boundRect;
+        boundRect = findRect(hsvinput, HueMin, HueMax, SatMin, SatMax, ValMin, ValMax);
+        boundRect = adjustRect(boundRect, 1.33);
+        resizeRect(boundRect, .2, hsvinput);
+        Mat boundMat = input(boundRect).clone();
+        imshow("boundMat", boundMat);
 
         ///  Get the mass centers:
         vector<Point2f> mc( contours.size() );
@@ -167,12 +261,12 @@ int main() {
         Mat drawing = Mat::zeros( temp.size(), CV_8UC3 );
         for( int i = 0; i< contours.size(); i++ )
         {
-            std::cout<<contours.size()<<std::endl;
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-            drawContours( input, contours, i, color, 2, 8, hierarchy, 0, Point() );
+            //drawContours( input, contours, i, color, 2, 8, hierarchy, 0, Point() );
             circle( input, mc[i], 4, color, -1, 8, 0 );
         }
-
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        rectangle( input, boundRect, color ,2 );
         //imshow("temp", temp);
         imshow("Edges", input);
         count++;
